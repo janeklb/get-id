c6a3c152930f5c949bea9734fbe9758f3eb6b8d0
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2016-09-23 10:16:32 +0100

    weeee tidy up

--- get-id.js
+++ get-id.js
@@ -8,17 +8,13 @@ var default_key = '__object_id__';
 var idcounter = 0;
 
 function create(id_key) {
-	return function get_id(object) {
-
+	return function (object) {
 		if (object[id_key] === undefined) {
 			Object.defineProperty(object, id_key, { value: ++idcounter });
 		}
-
 		return object[id_key];
 	};
 }
 
-var get_id = create(default_key);
-
-exports.default = get_id;
+exports.default = create(default_key);
 exports.create = create;
--- src/get-id.js
+++ src/get-id.js
@@ -3,17 +3,13 @@ const default_key = '__object_id__';
 var idcounter = 0;
 
 function create(id_key) {
-	return function get_id(object) {
-
+	return (object) => {
 		if (object[id_key] === undefined) {
 			Object.defineProperty(object, id_key, { value: ++idcounter });
 		}
-
 		return object[id_key];
 	};
 }
 
-const get_id = create(default_key);
-
-export default get_id;
+export default create(default_key);
 export { create };
